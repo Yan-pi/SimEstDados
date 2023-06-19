1fa570ba1dc7847bba1b0640b3297d51aa17f6e1
Yan Fernandes <102800044+Yan-pi@users.noreply.github.com>
2023-06-19 00:07:38 -0300

    Update script.js

--- listaEncadeada/script.js
+++ listaEncadeada/script.js
@@ -1,21 +1,26 @@
+// Definindo a classe Node que representa cada nó da lista encadeada
 class Node {
     constructor(value) {
-        this.value = value;
-        this.next = null;
+        this.value = value; // Valor armazenado no nó
+        this.next = null; // Referência para o próximo nó
     }
 }
 
+// Definindo a classe LinkedList que representa a lista encadeada
 class LinkedList {
     constructor() {
-        this.head = null;
+        this.head = null; // Referência para o primeiro nó (cabeça da lista)
     }
 
+    // Método para adicionar um novo nó na lista encadeada
     addNode(value) {
-        const newNode = new Node(value);
+        const newNode = new Node(value); // Cria um novo nó com o valor fornecido
 
         if (!this.head) {
+            // Se a lista estiver vazia, o novo nó se torna a cabeça da lista
             this.head = newNode;
         } else {
+            // Se a lista já tiver nós, percorre até o último nó e adiciona o novo nó no final
             let currentNode = this.head;
             while (currentNode.next !== null) {
                 currentNode = currentNode.next;
@@ -23,37 +28,42 @@ class LinkedList {
             currentNode.next = newNode;
         }
 
-        this.displayList();
+        this.displayList(); // Atualiza a exibição da lista
     }
 
+    // Método para remover um nó da lista encadeada
     deleteNode(value) {
-        if (!this.head) return;
+        if (!this.head) return; // Se a lista estiver vazia, não há nada a ser removido
 
         if (this.head.value === value) {
+            // Se o valor está na cabeça da lista, atualiza a cabeça para o próximo nó
             this.head = this.head.next;
-            this.displayList();
+            this.displayList(); // Atualiza a exibição da lista
             return;
         }
 
         let currentNode = this.head;
         while (currentNode.next !== null) {
             if (currentNode.next.value === value) {
+                // Se o próximo nó tiver o valor a ser removido, atualiza as referências para ignorar esse nó
                 currentNode.next = currentNode.next.next;
-                this.displayList();
+                this.displayList(); // Atualiza a exibição da lista
                 return;
             }
             currentNode = currentNode.next;
         }
     }
 
+    // Método para atualizar o valor de um nó da lista encadeada
     updateNode(index, value) {
         let count = 0;
         let currentNode = this.head;
 
         while (currentNode !== null) {
             if (count === index) {
+                // Quando o índice desejado é encontrado, atualiza o valor do nó
                 currentNode.value = value;
-                this.displayList();
+                this.displayList(); // Atualiza a exibição da lista
                 return;
             }
             currentNode = currentNode.next;
@@ -61,25 +71,27 @@ class LinkedList {
         }
     }
 
+    // Método para exibir a lista encadeada na página
     displayList() {
         let currentNode = this.head;
         let listHTML = "";
 
         while (currentNode !== null) {
-            listHTML += `<div class="node">${currentNode.value}</div>`;
+            listHTML += `<div class="node">${currentNode.value}</div>`; // Cria um elemento div para cada nó
             if (currentNode.next !== null) {
-                listHTML += `<span class="arrow">-></span>`;
+                listHTML += `<span class="arrow">-></span>`; // Adiciona uma seta entre os nós
             }
             currentNode = currentNode.next;
         }
 
         const listContainer = document.getElementById("list");
-        listContainer.innerHTML = listHTML;
+        listContainer.innerHTML = listHTML; // Insere o HTML gerado na página
     }
 }
 
-const linkedList = new LinkedList();
+const linkedList = new LinkedList(); // Cria uma nova instância da lista encadeada
 
+// Event listener para o botão "Push" (adicionar um novo nó)
 document.getElementById("btn-add").addEventListener("click", () => {
     const valueInput = document.getElementById("valueInput");
     const value = valueInput.value;
@@ -89,6 +101,7 @@ document.getElementById("btn-add").addEventListener("click", () => {
     }
 });
 
+// Event listener para o botão "Pop" (remover um nó)
 document.getElementById("btn-delete").addEventListener("click", () => {
     const valueInput = document.getElementById("valueInput");
     const value = valueInput.value;
@@ -98,6 +111,7 @@ document.getElementById("btn-delete").addEventListener("click", () => {
     }
 });
 
+// Event listener para o botão "Set" (atualizar um nó)
 document.getElementById("btn-update").addEventListener("click", () => {
     const valueInput = document.getElementById("valueInput");
     const value = valueInput.value;
